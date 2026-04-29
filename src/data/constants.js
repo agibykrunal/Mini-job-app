92f40dca6c599c3458583bc725aa69c586af33f4
Krunal <nsundersia@gmail.com>
2026-04-29 08:57:20 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,4 +1,4 @@
-//iiiiinhhhhhhnnhhhshhshs
+//iiiiinhhhhhhnnhhhshhsh
 export const COMPANIES = [
   { name: "Google", logo: "G", logoBg: "#4285F4", logoColor: "#fff" },
   { name: "Meta", logo: "M", logoBg: "#0866FF", logoColor: "#fff" },
